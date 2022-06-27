808b2a447c282334b3b159ec446d0aeb2c91ebcf
qaiik <82181231+qaiik@users.noreply.github.com>
2022-06-26 21:34:11 -0600

    Create rdc.js

---
+++ rdc.js
@@ -0,0 +1,4 @@
+let script = document.createElement("script")
+script.src="https://raw.githubusercontent.com/qaiik/rawdlchat/main/raw.js"
+script.type = "text/plain"
+document.head.appendChild(script)
